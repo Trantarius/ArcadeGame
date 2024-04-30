b9be3854f3398cf5105ca77e17d695394b8631e6
Trantarius <leborjt@yahoo.com>
2024-04-30 13:57:53 -0400

    actor fully custom integrator

--- Actor.gd
+++ Actor.gd
@@ -7,6 +7,8 @@ extends RigidBody2D
 enum ControlMode{
 	## Target is the desired thrust value (will be clamped to max thrust before being applied).
 	THRUST,
+	## Target is the desired acceleration. Thrust will automatically be set to try to reach that acceleration.
+	ACCELERATION,
 	## Target is the desired velocity. Thrust will automatically be set to try to reach that velocity.
 	VELOCITY,
 	## Target is the desired position. Thrust will automatically be set to try to reach that position (and stop there, if brake is true).
@@ -18,10 +20,17 @@ enum ControlMode{
 @export var max_linear_speed:float = 400
 ## Determines how linear_target will be used.
 @export var linear_control_mode:ControlMode = ControlMode.THRUST
-## When in position mode, thrust will be reversed to try to come to rest at the target position. Has no effect when linear_control_mode != POSITION.
-@export var linear_brake:bool = true
+## When in position mode, controls how much thrust will be reversed to try to come to rest at the target position.
+@export var linear_brake:float = 1
 ## See [member Actor.linear_control_mode]
 var linear_target:Vector2
+
+## Performs linear calculations in a reference frame defined by this position.
+var reference_position:Vector2
+## Performs linear calculations in a reference frame defined by this velocity.
+var reference_velocity:Vector2
+## Performs linear calculations in a reference frame defined by this acceleration.
+var reference_acceleration:Vector2
 
 ## Maximum self-applied torque.
 @export var max_angular_thrust:float = 100
@@ -29,10 +38,19 @@ var linear_target:Vector2
 @export var max_angular_speed:float = 3
 ## Determines how angular_target will be used.
 @export var angular_control_mode:ControlMode = ControlMode.THRUST
-## When in position mode, thrust will be reversed to try to come to rest at the target position. Has no effect when angular_control_mode != POSITION.
-@export var angular_brake:bool = true
+## When in position mode, controls how much thrust will be reversed to try to come to rest at the target position.
+@export var angular_brake:float = 1
 ## See [member Actor.angular_control_mode]
 var angular_target:float
+
+## Linear thrust applied last physics frame.
+var linear_thrust:Vector2
+## Linear acceleration applied last physics frame (excluding collisions).
+var linear_acceleration:Vector2
+## Angular thrust applied last physics frame.
+var angular_thrust:float
+## Angular acceleration applied last physics frame (excluding collisions).
+var angular_acceleration:float
 
 signal death(damage:Damage)
 signal kill(damage:Damage)
@@ -42,6 +60,7 @@ signal damage_dealt(damage:Damage)
 func _init()->void:
 	contact_monitor=true
 	max_contacts_reported=2
+	custom_integrator=true
 
 func take_damage(damage:Damage)->void:
 	damage.target=self
@@ -57,62 +76,94 @@ func take_damage(damage:Damage)->void:
 			damage.attacker.kill.emit(damage)
 		queue_free()
 
-func predict_position(delta:float)->Vector2:
-	return position + linear_velocity*delta + (constant_force/mass)*delta*delta/2
-
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	
+	var current_position:Vector2 = state.transform.origin - reference_position
+	var current_velocity:Vector2 = state.linear_velocity - reference_velocity
+	var current_acceleration:Vector2 = state.get_constant_force()*state.inverse_mass - reference_acceleration
+	
+	var solve_accel:Callable = func solve_accel(wdir:Vector2)->Dictionary:
+		var cw:float = current_acceleration.dot(wdir)
+		var perp:Vector2 = current_acceleration - cw*wdir
+		var t_maxacc:float = max_linear_thrust*state.inverse_mass
+		var ret:Dictionary = {}
+		if(t_maxacc**2<perp.length_squared()):
+			ret['success']=false
+			# thrust won't be able to overcome current acc, just get as close as possible
+			if(current_acceleration.dot(wdir)>0):
+				ret['thrust'] = max_linear_thrust*-perp.normalized()
+			else:
+				ret['thrust'] = max_linear_thrust*-current_acceleration.normalized()
+		else:
+			ret['success'] = true
+			ret['final_acc'] = sqrt(t_maxacc**2 - perp.length_squared())+abs(cw)
+			var t_acc:Vector2 = wdir*(ret.final_acc)-current_acceleration
+			ret['thrust'] = t_acc/state.inverse_mass
+		return ret
 	
 	match(linear_control_mode):
 		ControlMode.THRUST:
-			state.set_constant_force(linear_target.limit_length(max_linear_thrust))
+			linear_thrust = linear_target
+		
+		ControlMode.ACCELERATION:
+			linear_thrust = (linear_target - current_acceleration)/state.inverse_mass
 		
 		ControlMode.VELOCITY:
-			var thrust:Vector2 = (linear_target.limit_length(max_linear_speed)-state.linear_velocity).normalized()*max_linear_thrust
-			state.set_constant_force(thrust)
+			linear_thrust = solve_accel.call((linear_target-current_velocity).normalized()).thrust
+			linear_thrust = linear_thrust.limit_length((linear_target-current_velocity).length()/state.step/state.inverse_mass)
 		
 		ControlMode.POSITION:
-			if(linear_brake):
 				
-				var accel:float = max_linear_thrust*state.inverse_mass
-				var brake_time:float = state.linear_velocity.length() / accel
-				var anticipated_position:Vector2 = position + state.linear_velocity*brake_time/2
-				var correction_dir:Vector2 = (linear_target-anticipated_position).normalized()
-				var thrust:Vector2 = (correction_dir-state.linear_velocity.normalized()/2).normalized()*max_linear_thrust
-				state.set_constant_force(thrust)
+			var brake:Dictionary = solve_accel.call(-current_velocity.normalized())
+			if(!brake.success):
+				# braking is impossible
+				linear_thrust = brake.thrust
 			else:
+				var brake_acc:float = brake.final_acc
+				var brake_time:float = linear_brake*current_velocity.length()/brake_acc
+				var anticipated_position:Vector2 = current_position + current_velocity*brake_time
+				var correction_dir:Vector2 = (linear_target-anticipated_position).normalized()
 				
-				var xlen:float = (linear_target-position).length()
-				if(xlen>0.01):
-					var xdir:Vector2 = (linear_target-position)/xlen
-					var accel:float = max_linear_thrust*state.inverse_mass
-					
-					# 0 = (1/2) a t^2 + vi t + -d
-					# (-b + sqrt(b*b - 4*a*c))/(2*a)
-					var vi:float = state.linear_velocity.dot(xdir)
-					var xtime:float = (sqrt(vi*vi + 2*accel*xlen)-vi)/accel
-					
-					var ytime:float = abs(state.linear_velocity.dot(xdir.orthogonal()))*2/accel
-					var tdir:Vector2 = xtime*xdir - ytime*sign(state.linear_velocity.dot(xdir.orthogonal()))*xdir.orthogonal()
-					var thrust:Vector2 = tdir.normalized()*max_linear_thrust
-					state.set_constant_force(thrust)
+				linear_thrust = solve_accel.call((correction_dir-current_velocity.normalized()/2).normalized()).thrust
+				
+	
+	var current_angular_acceleration:float = state.get_constant_torque()*state.inverse_inertia
 	
 	match(angular_control_mode):
 		ControlMode.THRUST:
-			state.set_constant_torque(clamp(angular_target,-max_angular_thrust,max_angular_thrust))
+			angular_thrust = angular_target
+		
+		ControlMode.ACCELERATION:
+			angular_thrust = (angular_target-current_angular_acceleration)/state.inverse_inertia
 		
 		ControlMode.VELOCITY:
-			var thrust:float = sign(clamp(angular_target,-max_angular_speed,max_angular_speed)-state.angular_velocity)*max_angular_thrust
-			state.set_constant_torque(thrust)
+			angular_thrust = (angular_target-state.angular_velocity)/state.step/state.inverse_inertia
 		
 		ControlMode.POSITION:
-			if(angular_brake):
-				var accel:float = max_angular_thrust*state.inverse_inertia
-				var brake_time:float = abs(state.angular_velocity) / accel
-				var anticipated_position:float = rotation + state.angular_velocity*brake_time/2 
-				var thrust:float = sign(angle_difference(anticipated_position,angular_target))*max_angular_thrust
-				state.set_constant_torque(thrust)
-			else:
-				state.set_constant_torque(-max_angular_thrust*sign(angle_difference(angular_target,rotation)))
+			var accel:float = max_angular_thrust*state.inverse_inertia + current_angular_acceleration*-sign(state.angular_velocity)
+			var brake_time:float = angular_brake*abs(state.angular_velocity) / accel
+			var anticipated_position:float = rotation + state.angular_velocity*brake_time
+			angular_thrust = sign(-angle_difference(angular_target,anticipated_position))*max_angular_thrust
 	
-	state.linear_velocity = state.linear_velocity.limit_length(max_linear_speed)
+	linear_thrust = linear_thrust.limit_length(max_linear_thrust)
+	var linear_force:Vector2 = linear_thrust + state.get_constant_force()
+	state.set_constant_force(Vector2.ZERO)
+	linear_acceleration = linear_force * state.inverse_mass
+	
+	linear_acceleration += state.linear_velocity
+	linear_acceleration = linear_acceleration.limit_length(max_linear_speed)
+	linear_acceleration -= state.linear_velocity
+	
+	state.linear_velocity += linear_acceleration * state.step
+	position += state.linear_velocity * state.step
+	
+	angular_thrust = clamp(angular_thrust, -max_angular_thrust, max_angular_thrust)
+	var angular_force:float = angular_thrust + state.get_constant_torque()
+	state.set_constant_torque(0)
+	angular_acceleration = angular_force * state.inverse_inertia
+	var old_ang_vel:float = state.angular_velocity
+	state.angular_velocity += angular_acceleration * state.step
 	state.angular_velocity = clamp(state.angular_velocity,-max_angular_speed,max_angular_speed)
+	angular_acceleration = (state.angular_velocity-old_ang_vel)/state.step
+	rotation += state.angular_velocity * state.step
+	
