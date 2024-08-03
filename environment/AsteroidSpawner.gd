bb7943cb1b9156e845282ce8c4b392791ebc7a80
Trantarius <leborjt@yahoo.com>
2024-08-03 17:22:27 -0400

    main menu asteroids

--- environment/AsteroidSpawner.gd
+++ environment/AsteroidSpawner.gd
@@ -17,49 +17,87 @@ extends Node
 @export var spawn_distance:float = 4000
 ## Distance from player to despawn asteroids
 @export var despawn_distance:float = 5000
+## Makes asteroids move towards the screen center (<0 to disable)
+@export var motion_target_radius:float = -1
 
 func _ready()->void:
-	var player:Player = get_tree().get_first_node_in_group('Players')
-	if(player==null):
-		return
 	for n:int in range(asteroid_count):
-		spawn_asteroid(func()->Transform2D: return Transform2D(randf()*TAU,
-			player.position + Vector2(randf_range(-despawn_distance,spawn_distance),randf_range(-despawn_distance,spawn_distance))))
+		await spawn_asteroid_anywhere()
 
 func _physics_process(_delta: float) -> void:
-	var centerpos:Vector2 = get_viewport().get_camera_2d().get_screen_center_position()
+	var centerpos:Vector2 = Util.current_camera_pos()
 	
-	for child:Node in get_children():
-		var relpos:Vector2 = child.position - centerpos
-		if(relpos.x < -despawn_distance):
-			spawn_asteroid(func()->Transform2D: return Transform2D(randf()*TAU,
-				centerpos + Vector2(spawn_distance,(randf()-0.5)*despawn_distance)))
-			child.queue_free()
-		elif(relpos.x > despawn_distance):
-			spawn_asteroid(func()->Transform2D: return Transform2D(randf()*TAU,
-				centerpos + Vector2(-spawn_distance,(randf()-0.5)*despawn_distance)))
-			child.queue_free()
-		elif(relpos.y < -despawn_distance):
-			spawn_asteroid(func()->Transform2D: return Transform2D(randf()*TAU,
-				centerpos + Vector2((randf()-0.5)*despawn_distance,spawn_distance)))
-			child.queue_free()
-		elif(relpos.y > despawn_distance):
-			spawn_asteroid(func()->Transform2D: return Transform2D(randf()*TAU,
-				centerpos + Vector2((randf()-0.5)*despawn_distance,-spawn_distance)))
+	for child:Asteroid in get_children():
+		if((child.position-centerpos).length()-child.radius>despawn_distance):
+			spawn_asteroid_dir(centerpos-child.position)
 			child.queue_free()
 
-func spawn_asteroid(locator:Callable)->void:
-		
+func spawn_asteroid_dir(dir:Vector2)->void:
 	var aster:Asteroid = preload("res://environment/asteroid.tscn").instantiate()
-	aster.radius = mean_size * (2**randfn(0,size_variation))
+	var svar:float = randfn(0,size_variation)
+	svar = sign(svar) * fposmod(abs(svar),2*size_variation)
+	aster.radius = mean_size * 2**svar
 	aster.noise.seed = randi()
-	aster.mass = (aster.radius/50)**3
+	aster.mass = (aster.radius/50)**2
+	await aster.generate()
 	
-	if(!Util.attempt_place_node(aster,self,locator,5)):
-		aster.queue_free()
-		push_error("Failed to place an asteroid after 5 attempts")
-	else:
+	var tform:Transform2D = Transform2D(randf()*TAU, Util.current_camera_pos() + 
+		Vector2.from_angle(dir.angle()+randfn(0,0.5)) * (spawn_distance + aster.radius + randf()*(despawn_distance-spawn_distance)))
+	while(!Util.does_node_fit(aster, tform)):
+		await get_tree().physics_frame
+		svar = randfn(0,size_variation)
+		svar = sign(svar) * fposmod(abs(svar),2*size_variation)
+		aster.radius = mean_size * 2**svar
+		aster.noise.seed = randi()
+		aster.mass = (aster.radius/50)**2
+		await aster.generate()
+		tform = Transform2D(randf()*TAU, Util.current_camera_pos() + 
+			Vector2.from_angle(dir.angle()+randfn(0,0.5)) * (spawn_distance + aster.radius + randf()*(despawn_distance-spawn_distance)))
+	
+	aster.transform = tform
+	add_child.call_deferred(aster)
+	await aster.tree_entered
+	
+	if(motion_target_radius<0):
 		aster.apply_central_impulse(Vector2.from_angle(randf()*TAU) * max_linear_momentum * (2**randfn(0,1)))
-		aster.apply_torque_impulse(randfn(-1,1) * max_angular_momentum * (2**randfn(0,1)))
+	else:
+		var target:Vector2 = Vector2.from_angle(randf()*TAU)*sqrt(randf())*motion_target_radius + Util.current_camera_pos()
+		var momentum:Vector2 = (target - aster.position).normalized() * max_linear_momentum * (2**randfn(0,1))
+		aster.apply_central_impulse(momentum)
+		
+	aster.apply_torque_impulse(randfn(-1,1) * max_angular_momentum * (2**randfn(0,1)))
+
+func spawn_asteroid_anywhere()->void:
+	var aster:Asteroid = preload("res://environment/asteroid.tscn").instantiate()
+	var svar:float = randfn(0,size_variation)
+	svar = sign(svar) * fposmod(abs(svar),2*size_variation)
+	aster.radius = mean_size * 2**svar
+	aster.noise.seed = randi()
+	aster.mass = (aster.radius/50)**2
+	await aster.generate()
 	
+	var tform:Transform2D = Transform2D(randf()*TAU, Util.current_camera_pos() + 
+		Vector2.from_angle(randf()*TAU) * sqrt(randf()) * ((spawn_distance+despawn_distance)/2 + aster.radius))
+	while(!Util.does_node_fit(aster, tform)):
+		await get_tree().physics_frame
+		svar = randfn(0,size_variation)
+		svar = sign(svar) * fposmod(abs(svar),2*size_variation)
+		aster.radius = mean_size * 2**svar
+		aster.noise.seed = randi()
+		aster.mass = (aster.radius/50)**2
+		await aster.generate()
+		tform = Transform2D(randf()*TAU, Util.current_camera_pos() + 
+			Vector2.from_angle(randf()*TAU) * sqrt(randf()) * ((spawn_distance+despawn_distance)/2 + aster.radius))
 	
+	aster.transform = tform
+	add_child.call_deferred(aster)
+	await aster.tree_entered
+	
+	if(motion_target_radius<0):
+		aster.apply_central_impulse(Vector2.from_angle(randf()*TAU) * max_linear_momentum * (2**randfn(0,1)))
+	else:
+		var target:Vector2 = Vector2.from_angle(randf()*TAU)*sqrt(randf())*motion_target_radius + Util.current_camera_pos()
+		var momentum:Vector2 = (target - aster.position).normalized() * max_linear_momentum * (2**randfn(0,1))
+		aster.apply_central_impulse(momentum)
+		
+	aster.apply_torque_impulse(randfn(-1,1) * max_angular_momentum * (2**randfn(0,1)))
